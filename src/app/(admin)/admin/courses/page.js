ca018aa3fcc5b5410f19983d1aacd8362723c2e2
ahmedullahmern <ahmedullahmern@gmail.com>
2024-10-27 12:30:41 +0500

    add course table and packeages

--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -1,7 +1,10 @@
+import { CourseDataTable } from "@/components/ui/DataTable/CourseTable";
+
 export default function Courses() {
     return (
         <div className="text-center p-10">
             <h1 className="font-bold text-4xl">Courses</h1>
+            <CourseDataTable />
         </div>
     )
 }
