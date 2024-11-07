80b3baaca4e025f49e65ffb4c77a4ca148603956
ahmedullahmern <ahmedullahmern@gmail.com>
2024-11-07 23:14:58 +0500

    connect database

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,12 +1,34 @@
 import { connectDB } from "@/lib/dbConnect";
-
-
+import { userModal } from "@/lib/modals/UserModal";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export async function POST(request) {
     await connectDB();
     const obj = await request.json();
     console.log("obj==>", obj)
-    return Response.json("User Post Request ")
+
+    // Check  User Exeist
+    const userExist = await userModal.findOne({ email: obj.email })
+    if (userExist) return Response.json({ error: true, msg: "User Already Exits" }, { status: 403 })
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(obj.password, saltRounds)
+    obj.password = hashedPassword
+
+    let newUser = new userModal({ ...obj })
+    newUser = await newUser.save()
+
+    const token = jwt.sign({ id: newUser._id, role: newUser.role }, process.env.JWT_KEY);
+
+    console.log("obj.password==>", obj.password)
+    // console.log("hashedPassword==>", hashedPassword)
+    return Response.json({
+        error: false,
+        msg: "User Add Successlly",
+        user: newUser,
+        token
+    })
 }
 
 
--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -7,9 +7,9 @@ export async function connectDB() {
         console.log("connection?.connection", connection?.connection)
         if (connection?.connection?.readyState !=1) {
             connection = await mongoose.connect(process.env.MONGODB_URI)
-            console.log("DB connected..")
+            console.log("DB connected ...")
         }
     } catch (err) {
-        console.log("error Sahab kiu nahi bhiya ji Smile==>", err)
+        console.log("error not connect ==>", err)
     }
 }
